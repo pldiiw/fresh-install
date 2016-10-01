22ba1d024285a5f83dada3dc14fb7dadf3c761c2
PLD <pierre-louis@dourneau.me>
2016-10-01 23:03:16 +0200

    Installation across reboots now works properly
    Make home restore do not prompt overwrite, fix == comparisons in
    fresh-install.sh and use repo absolute path to enable resuming of
    installation after one of the steps ordered a reboot to work properly.

--- steps/3.sh
+++ steps/3.sh
@@ -2,7 +2,7 @@
 bup -d /media/pld/Prout/bup restore home-pld/latest/home/pld -C /home/pld/buptmp
 
 # cp latest restores bup to home
-cp -fR /home/pld/buptmp/pld /home
+\cp -R /home/pld/buptmp/pld /home
 
 # clean
 rm -rf /home/pld/buptmp
